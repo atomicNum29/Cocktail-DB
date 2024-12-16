97a955fdcec67232e1342652e136f20ffd81a53e
atomicNum29 <atomicw63.546@gmail.com>
2024-12-16 21:10:14 +0900

    칵테일 상세 보기 페이지 작성

--- app.py
+++ app.py
@@ -18,6 +18,19 @@ def index():
     conn.close()
     return render_template('index.html', cocktails=cocktails)
 
+# 특정 칵테일의 상세 정보
+@app.route('/cocktail/<int:cocktail_id>')
+def cocktail_detail(cocktail_id):
+    conn = get_db_connection()
+    cocktail = conn.execute('SELECT * FROM Cocktail WHERE id = ?', (cocktail_id,)).fetchone()
+    ingredients = conn.execute("""
+        SELECT i.name, ci.amount
+        FROM Ingredient i
+        JOIN Cocktail_Ingredient ci ON i.id = ci.ingredient_id
+        WHERE ci.cocktail_id = ?
+    """, (cocktail_id,)).fetchall()
+    conn.close()
+    return render_template('cocktail_detail.html', cocktail=cocktail, ingredients=ingredients)
 
 if __name__ == '__main__':
     app.debug = True
